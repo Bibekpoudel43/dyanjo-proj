6c3f88bb7fad68df4771a66547381488f8079d1b
Bibekpoudel43 <bibekpoudel43@gmail.com>
2020-04-08 09:11:46 +0930

    profile added and changes in views file

--- signup/users/views.py
+++ signup/users/views.py
@@ -1,20 +1,47 @@
+from http import HTTPStatus
+
 from django.shortcuts import render, redirect
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User, auth
+from django.http import HttpResponse
 
 
 # Create your views here.
+def login(request):
+    if request.method == "POST":
+        print('faffed')
+
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = auth.authenticate(username=username, password=password)
+
+        if user is not None:
+            auth.login(request, user)
+
+            return redirect('/users')
+        else:
+            return redirect('login')
+    else:
+        return render(request, 'login.html')
+
+
 def register(request):
-    if request.method == 'POST':
+    if request.method == "POST":
         username = request.POST['username']
         email = request.POST['email']
         password = request.POST['password']
+        password2 = request.POST['password2']
+
         user = User.objects.create_user(username=username, email=email, password=password)
         user.save()
-        print('user created')
-        return redirect('/')
+        return redirect('login')
     else:
         return render(request, 'register.html')
 
 
 def home_view(request, *args, **kwargs):
     return render(request, 'index.html')
+
+
+def profile(request, *args, **kwargs):
+    return render(request, 'profile.html')
