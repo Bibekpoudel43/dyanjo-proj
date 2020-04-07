5fbcaf7229bd88fbe7a234ac2f8c15ce10e5e445
Bibekpoudel43 <bibekpoudel43@gmail.com>
2020-04-08 09:12:20 +0930

    login and profile url

--- signup/users/urls.py
+++ signup/users/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.home_view, name='home_view'),
     path("register", views.register, name="register"),
+    path("login", views.login, name="login"),
+    path("profile", views.profile, name="profile"),
 ]
