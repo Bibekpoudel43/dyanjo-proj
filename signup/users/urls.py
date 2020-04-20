ba2a8a3ad2aec021e0f77d3e001eca6ecf332420
Bibekpoudel43 <bibekpoudel43@gmail.com>
2020-04-20 17:19:37 +0930

    added new urls

--- signup/users/urls.py
+++ signup/users/urls.py
@@ -6,4 +6,7 @@ urlpatterns = [
     path("register", views.register, name="register"),
     path("login", views.login, name="login"),
     path("profile", views.profile, name="profile"),
+    path("device", views.device, name="device"),
+    path("about", views.about, name="about"),
+    path("library", views.library, name="library"),
 ]
