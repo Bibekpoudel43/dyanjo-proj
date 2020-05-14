3f1027337a9c06a0735fd62a2a55ae4362a2074d
Bibekpoudel43 <bibekpoudel43@gmail.com>
2020-05-14 18:50:16 +0930

    filesystem added

--- signup/users/views.py
+++ signup/users/views.py
@@ -8,7 +8,6 @@ from django.core.files.storage import FileSystemStorage
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
 from django.contrib.auth import logout
-from django.core.exceptions import ValidationError
 from django.http import HttpResponseRedirect
 
 
